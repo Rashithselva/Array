2dff112bd6ca44d9243f1e602c2ceb0aed8c4347
Rashithselva <ravirashith@gmail.com>
2023-12-19 19:04:39 +0530

    variable declaration outside the for loop

--- Bubblesort.java
+++ Bubblesort.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 public class Bubblesort {
     public static void main(String[] args) {
+
         int []a={8,9,7,8,6,5,4,3,2,1};
+        int temp=0;
+        
         for(int i=1;i<a.length;i++){
             for(int j=0;j<a.length-i;j++){
                 if(a[j]>a[j+1]){
-                    int temp=a[j];
+                     temp=a[j];
                     a[j]=a[j+1];
                     a[j+1]=temp;
                 }
