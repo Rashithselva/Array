2f1f9e209c7de3c36b1fd6424f3bcfc246baf148
Rashithselva <ravirashith@gmail.com>
2023-12-20 18:06:03 +0530

    program added

--- NthLargestnum.java
+++ NthLargestnum.java
@@ -1,5 +1,30 @@
 package Array;
+import java.util.*;
 
 public class NthLargestnum {
+    public static void main(String[] args) {
+        Scanner r=new Scanner (System.in);
+
+        System.out.println("Enter the largest number you want 1 to 6");
+
+        int a[]={1,2,3,4,5,6};
+        int largest=r.nextInt();
+        int large=a[0];
+
+        for(int i=1;i<=largest;i++){
+            large=a[0];
+            for(int j=0;j<a.length;j++){
+                if(a[j]>large){
+                    large=a[j];
+                }
+            }
+            for(int j=0;j<a.length;j++){
+                if(a[j]==large){
+                    a[j]=-1;
+                }
+            }
+        }
+        System.out.println(large);
+    }
     
 }
