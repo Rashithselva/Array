7c2446a1fd2afba8f66c1c513218d695f47434d8
Rashithselva <ravirashith@gmail.com>
2023-12-22 18:30:22 +0530

    array size is user wish

--- Userinput.java
+++ Userinput.java
@@ -1,5 +1,12 @@
 package Array;
+import java.util.*;
 
 public class Userinput {
+    public static void main(String[] args) {
+        Scanner r=new Scanner(System.in);
+        System.out.println("Enter tye size of an array");
+        int []a=new int[r.nextInt()];
+        
+    }
     
 }
