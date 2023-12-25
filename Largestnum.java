3a12cc68e9edae751fa6ab3ebaa2d09ce703ec72
Rashithselva <ravirashith@gmail.com>
2023-12-25 19:24:22 +0530

    Modifi to user wish

--- Largestnum.java
+++ Largestnum.java
@@ -1,16 +1,25 @@
 package Array;
+import java.util.*;
 
 public class Largestnum {
     public static void main(String[] args) {
-        
-        int a[]={1,2,3,4,5};
+        Scanner r=new Scanner(System.in);
+
+        System.out.println("Enter the size of an array");
+         int a[]=new int[r.nextInt()];
+         
+         for(int i=0;i<a.length;i++){
+            System.out.println("Enter the element you want");
+            a[i]=r.nextInt();
+         }
+
         int large=a[0];
         for(int i=0;i<a.length;i++){
             if(a[i]>large){
                 large=a[i];
             }
         }
-        System.out.println(large);
+        System.out.println("Largest Element:"+large);
     }
     
 }
